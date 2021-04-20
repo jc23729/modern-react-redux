6dcf753eb10b371dec80edf108846e38c34dfcd2
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-20 15:24:49 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import ReactDOM from 'react-dom';
 
 //Create a react component
 
-
+const App = () => {
+  <div>
+    <label className="label" for="name">Enter your name:</label>
+    <input id="name" type="text" />
+    <button style={{ backgroundColor: 'blue', color: 'white'}}>Submit</button>
+  </div>
+};
 
 
 //Take the react component and show it on the screen
 
 
-
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
 
 
