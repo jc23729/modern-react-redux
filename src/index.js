c5388f11414aec5fd96db8a26a86d69e2e399571
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-20 15:36:17 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,50 +1,29 @@
-//Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
-//Create a react component
+if (module.hot) {
+  module.hot.accept();
+}
 
+function getButtonText() {
+  return "Click on me!";
+}
+
+// Create a react component
 const App = () => {
-  <div>
-    <label className="label" for="name">Enter your name:</label>
-    <input id="name" type="text" />
-    <button style={{ backgroundColor: 'blue', color: 'white'}}>Submit</button>
-  </div>
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {getButtonText()}
+      </button>
+    </div>
+  );
 };
 
-
-//Take the react component and show it on the screen
-
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
